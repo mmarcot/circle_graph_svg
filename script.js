443ebc61531b0e0be0e2683b184298a6ee4468a6
Mallory MARCOT <mmarcot@gmail.com>
2014-09-29 14:31:47 +0200

    Codage de la methode drawCurrentObject

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function SVGClass(ref) {
   this.y_centre = this.height / 2;
 }
 
-
+/**
+ * Focntion qui déssine un cercle SVG
+ * @param  {int} cx         x centre
+ * @param  {int} cy         y centre
+ * @param  {int} radius     rayon en px
+ * @param  {colro} fill_color  La couleur
+ */
 function drawCircle(cx, cy, radius, fill_color) {
   var shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   shape.setAttributeNS(null, "cx", cx);
@@ -21,17 +27,30 @@ function drawCircle(cx, cy, radius, fill_color) {
   document.getElementById('hierarchy').appendChild(shape);
 }
 
-function drawCurrentObject(svg) {
 
+/**
+ * Fonction qui déssine l'objet courant sous forme de cercle SVG
+ * @param  {SVGClass} svg L'objet svg
+ */
+function drawCurrentObject(svg) {
+  var coef = 0.4;
+  drawCircle(svg.x_centre, svg.y_centre, (svg.cote_min/2)*coef, "red");
 }
 
+/**
+ * Fonction qui desiine les parents sous forme de cercle
+ * @param  {SVGClass} svg        L'objet svg
+ * @param  {int} nb_parents Le nombre de parent de l'objet courant
+ */
 function drawParents(svg, nb_parents) {
   drawCircle(svg.x_centre, svg.y_centre, svg.cote_min/2, "green");
 }
 
+
 function drawSiblings(svg, nb_siblings) {
 
 }
+
 
 function drawChildren(svg, nb_children) {
 
